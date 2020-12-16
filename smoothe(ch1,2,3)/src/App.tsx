56f832c0e48c64baf7462216339286dea083f860
kasumil <ridou99@gmail.com>
2020-12-16 23:21:48 +0900

    [git] 브랜치 정리

---
+++ smoothe(ch1,2,3)/src/App.tsx
@@ -0,0 +1,126 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { SafeAreaView, StatusBar } from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import styled from "styled-components";
+
+const ScrollView = styled.ScrollView`
+  background-color: ${Colors.lighter};
+`;
+
+const Body = styled.View`
+  background-color: ${Colors.white};
+`;
+
+const SectionContainer = styled.View`
+  margin-top: 32px;
+  padding-Horizontal: 24px;
+`;
+
+const SectionDescription = styled.Text`
+  margin-top: 8px;
+  font-size: 18px;
+  font-weight: 400;
+  color: ${Colors.dark};
+`;
+
+const HighLight = styled.Text`
+  font-weight: 700;
+`;
+
+interface Props {}
+
+const App = ({}: Props) => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView contentInsetAdjustmentBehavior="automatic">
+          <Header />
+          <Body>
+            <SectionContainer>
+              <SectionDescription>Step One</SectionDescription>
+              <SectionDescription>
+                Edit <HighLight>App.js</HighLight> to change this screen and
+                then come back to see your edits
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionDescription>See Your Changes</SectionDescription>
+              <SectionDescription>
+                <ReloadInstructions />
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionDescription>Debug</SectionDescription>
+              <SectionDescription>
+                <DebugInstructions />
+              </SectionDescription>
+            </SectionContainer>
+            <SectionContainer>
+              <SectionDescription>Learn More</SectionDescription>
+              <SectionDescription>
+                Read the docs to discover what to do next:
+              </SectionDescription>
+            </SectionContainer>
+            <LearnMoreLinks />
+          </Body>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+// const styles = StyleSheet.create({
+//   scrollView: {
+//     backgroundColor: Colors.lighter,
+//   },
+//   engine: {
+//     position: 'absolute',
+//     right: 0,
+//   },
+//   body: {
+//     backgroundColor: Colors.white,
+//   },
+//   sectionContainer: {
+//     marginTop: 32,
+//     paddingHorizontal: 24,
+//   },
+//   sectionTitle: {
+//     fontSize: 24,
+//     fontWeight: '600',
+//     color: Colors.black,
+//   },
+//   sectionDescription: {
+//     marginTop: 8,
+//     fontSize: 18,
+//     fontWeight: '400',
+//     color: Colors.dark,
+//   },
+//   highlight: {
+//     fontWeight: '700',
+//   },
+//   footer: {
+//     color: Colors.dark,
+//     fontSize: 12,
+//     fontWeight: '600',
+//     padding: 4,
+//     paddingRight: 12,
+//     textAlign: 'right',
+//   },
+// });
+
+export default App;
