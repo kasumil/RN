147fac63f9511040b6e6ff8cc5a19c1bffe2fe4c
kasumil <ridou99@gmail.com>
2020-12-22 02:37:10 +0900

    커밋

--- ch9/SNSapp/android/app/src/main/java/com/snsapp/MainActivity.java
+++ ch9/SNSapp/android/app/src/main/java/com/snsapp/MainActivity.java
@@ -5,15 +5,15 @@ import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.show(this, true);
-        super.onCreate(savedInstanceState);
-    }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    SplashScreen.show(this, true);
+    super.onCreate(savedInstanceState);
+  }
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule rendering of the component.
    */
   @Override
   protected String getMainComponentName() {
