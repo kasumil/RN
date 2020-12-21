7d9433a30d1646a1ce6869f14f8900f68f991b71
kasumil <ridou99@gmail.com>
2020-12-22 02:22:09 +0900

    add 강제저장

--- ch9/SNSApp/src/Context/RandomUserData/index.tsx
+++ ch9/SNSApp/src/Context/RandomUserData/index.tsx
@@ -20,5 +20,62 @@ const RandomUserDataContext = createContext<IRandomUserData>({
 });
 
 const RandomUserDataProvider = ({cache, children}:Props) => {
-  
-}
+  const [userList, setUserList] = useState<Array<IUserProfile>>([]);
+  const [description, setDescription] = useState<Array<string>>([]);
+  const [imageList, setImageList] = useState<Array<string>>([]);
+
+  const getCacheData = async (key: string) => {
+    const cacheData = await AsyncStorage.getItem('key');
+    if (cache === false || cacheData === null ) {
+      return undefined;
+    };
+
+    const cacheList = JSON.parse(cacheData);
+
+    if (cacheList.length !== 25 ) {
+      return undefined;
+    };
+    
+    return cacheList;
+  };
+
+  const setCacheData = (key: string, data: Array<any>) => {
+    AsyncStorage.setItem(key, JSON.stringify(data))
+  };
+
+  const setUsers = async () => {
+    const cacheData = await getCacheData('UserList');
+    if (cacheData) {
+      setUserList(cacheData);
+      return;
+    }
+
+    try {
+      const response = await fetch(
+        'https://raw.githubusercontent.com/dev-yakuza/users/master/api.json',
+      );
+      const data = await response.json();
+      setUserList(data);
+      setCacheData('UserList', data);
+    } catch (err) {
+      console.log(err);
+    };
+  };
+
+  const setDescription = async () => {
+    const cacheData = await getCacheData('DescriptionList');
+    console.log(cacheData);
+    if (cacheData) {
+      setDescription(cacheData);
+      return;
+    }
+    try {
+      const response = await fetch(
+        'https://opinionated-quotes-api.gigalixirapp.com/v1/quotes?rand=t&n=25',
+      );
+      const data = await response.json();
+      
+    }
+  }
+};
+
